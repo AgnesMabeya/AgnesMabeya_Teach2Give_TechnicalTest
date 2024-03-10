452d4ddca7d7e95b987afa82e606e0c158d8579a
AgnesMabeya <125739614+AgnesMabeya@users.noreply.github.com>
2024-03-10 12:45:44 +0300

    Add files via upload

---
+++ ReverseInteger.py
@@ -0,0 +1,11 @@
+#Question 5:Reverse Integer
+#Write a program that takes an integer and returns an integer with reversed digit ordering.
+def reverse_integer(n):
+    if n<0:
+        return -int(str(n)[1:][::-1])
+    else:
+        return int(str(n)[::-1])
+num=int(input("Enter an integer:"))
+reversed_num=reverse_integer(num)
+print(reversed_num)
+    
